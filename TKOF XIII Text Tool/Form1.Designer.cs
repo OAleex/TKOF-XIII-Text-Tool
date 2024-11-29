ff9f0a28113552f08eceab2ba8f562b3283b233e
OAleex <oaleex@hotmail.com>
2024-11-29 06:50:30 -0300

    Add Project

---
+++ TKOF XIII Text Tool/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TKOF_XIII_Text_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xORDecryptorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xOREncryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Translate Extract";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Translate Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(541, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xORDecryptorToolStripMenuItem,
+            this.xOREncryptToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // xORDecryptorToolStripMenuItem
+            // 
+            this.xORDecryptorToolStripMenuItem.Name = "xORDecryptorToolStripMenuItem";
+            this.xORDecryptorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.xORDecryptorToolStripMenuItem.Text = "XOR Decrypt";
+            this.xORDecryptorToolStripMenuItem.Click += new System.EventHandler(this.xORDecryptorToolStripMenuItem_Click);
+            // 
+            // xOREncryptToolStripMenuItem
+            // 
+            this.xOREncryptToolStripMenuItem.Name = "xOREncryptToolStripMenuItem";
+            this.xOREncryptToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.xOREncryptToolStripMenuItem.Text = "XOR Encrypt";
+            this.xOREncryptToolStripMenuItem.Click += new System.EventHandler(this.xOREncryptToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(277, 131);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(353, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(181, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nintendo Switch / PlayStation®4";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(353, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(148, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "PlayStation®3 / Xbox 360";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(353, 73);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(39, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "PC";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 235);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(557, 274);
+            this.MinimumSize = new System.Drawing.Size(557, 274);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "The King of Fighters XIII Text Tool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xORDecryptorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xOREncryptToolStripMenuItem;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ TKOF XIII Text Tool/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TKOF_XIII_Text_Tool
+{
+    public partial class Form1 : Form
+    {
+        private const byte XORKey = 0x66;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select the translate file",
+                Filter = "Lua File (*.lua)|*.lua"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                if (radioButton3.Checked)
+                {
+                    string tempDecryptedPath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_decrypted_temp.lua");
+
+                    EncryptFile(filePath, tempDecryptedPath);
+
+                    var extractedStrings = ExtractStringsFromFile(tempDecryptedPath);
+                    SaveStringsToFile(extractedStrings, filePath);
+
+                    File.Delete(tempDecryptedPath);
+
+                    MessageBox.Show($"File extracted successfully!", "Success!");
+                }
+                else
+                {
+                    var extractedStrings = ExtractStringsFromFile(filePath);
+                    SaveStringsToFile(extractedStrings, filePath);
+
+                    MessageBox.Show($"File extracted successfully!", "Success!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select the original translate file",
+                Filter = "Lua File (*.lua)|*.lua"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string originalFilePath = openFileDialog.FileName;
+
+                OpenFileDialog translationFileDialog = new OpenFileDialog
+                {
+                    Title = "Select the translation file",
+                    Filter = "Text File (*.txt)|*.txt"
+                };
+
+                if (translationFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string translationFilePath = translationFileDialog.FileName;
+
+                    if (radioButton3.Checked)
+                    {
+                        string tempDecryptedPath = Path.Combine(
+                            Path.GetDirectoryName(originalFilePath),
+                            Path.GetFileNameWithoutExtension(originalFilePath) + "_decrypted_temp.lua"
+                        );
+
+                        EncryptFile(originalFilePath, tempDecryptedPath);
+
+                        string tempUpdatedPath = tempDecryptedPath + ".new";
+                        InsertTranslatedStrings(tempDecryptedPath, translationFilePath);
+
+                        string encryptedOutputPath = Path.Combine(
+                            Path.GetDirectoryName(originalFilePath),
+                            Path.GetFileNameWithoutExtension(originalFilePath) + ".lua.new"
+                        );
+
+                        EncryptFile(tempUpdatedPath, encryptedOutputPath);
+
+                        File.Delete(tempDecryptedPath);
+                        File.Delete(tempUpdatedPath);
+
+                        MessageBox.Show($"Texts inserted and new file created successfully!", "Success!");
+                    }
+                    else
+                    {
+                        InsertTranslatedStrings(originalFilePath, translationFilePath);
+
+                        MessageBox.Show($"Texts inserted and new file created successfully!", "Success!");
+                    }
+                }
+            }
+        }
+
+        private List<string> ExtractStringsFromFile(string filePath)
+        {
+            List<string> strings = new List<string>();
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, data.Length);
+
+                int offset = 0;
+                while (offset < data.Length - 5)
+                {
+                    if (data[offset] == 0x04)
+                    {
+                        int length;
+                        if (radioButton2.Checked)
+                        {
+                            length = (data[offset + 1] << 24) |
+                                     (data[offset + 2] << 16) |
+                                     (data[offset + 3] << 8) |
+                                     data[offset + 4];
+                        }
+                        else
+                        {
+                            length = BitConverter.ToInt32(data, offset + 1);
+                        }
+
+                        if (length > 0 && offset + 5 + length <= data.Length)
+                        {
+                            byte[] stringBytes = new byte[length];
+                            Array.Copy(data, offset + 5, stringBytes, 0, length);
+
+                            string decodedString = Encoding.UTF8.GetString(stringBytes).TrimEnd('\0').Replace("\n", "<lw>");
+                            if (Regex.IsMatch(decodedString, @"^[\x20-\x7E<lw>\u00A0-\uFFFF]+$"))
+                            {
+                                strings.Add(decodedString);
+                            }
+                        }
+                    }
+                    offset++;
+                }
+            }
+            return strings;
+        }
+
+        private void SaveStringsToFile(List<string> strings, string filePath)
+        {
+            string outputFilePath = Path.ChangeExtension(filePath, ".txt");
+            File.WriteAllLines(outputFilePath, strings, Encoding.UTF8);
+        }
+
+        private void InsertTranslatedStrings(string originalFilePath, string translationFilePath)
+        {
+            try
+            {
+                var translatedStrings = File.ReadAllLines(translationFilePath, Encoding.UTF8);
+                byte[] data = File.ReadAllBytes(originalFilePath);
+
+                List<(int Offset, int Length, string OriginalString)> stringsWithOffsets = ExtractStringsWithOffsets(data);
+
+                using (MemoryStream updatedData = new MemoryStream())
+                {
+                    int lastOffset = 0;
+
+                    for (int i = 0; i < stringsWithOffsets.Count; i++)
+                    {
+                        var (offset, originalLength, originalString) = stringsWithOffsets[i];
+
+                        Console.WriteLine($"Processing string {i + 1}/{stringsWithOffsets.Count}");
+                        Console.WriteLine($"Offset: {offset}, Length: {originalLength}");
+
+                        if (offset < 0 || offset >= data.Length || offset + originalLength > data.Length)
+                        {
+                            throw new InvalidOperationException($"Invalid offset or length detected. Offset: {offset}, Length: {originalLength}, Data Length: {data.Length}");
+                        }
+
+                        updatedData.Write(data, lastOffset, offset - lastOffset);
+
+                        string newString = i < translatedStrings.Length ? translatedStrings[i].Replace("<lw>", "\n") : originalString;
+                        byte[] newStringBytes = Encoding.UTF8.GetBytes(newString);
+                        int newLength = newStringBytes.Length + 1;
+
+
+                        updatedData.WriteByte(0x04);
+
+                        if (radioButton2.Checked)
+                        {
+                            updatedData.WriteByte((byte)((newLength >> 24) & 0xFF));
+                            updatedData.WriteByte((byte)((newLength >> 16) & 0xFF));
+                            updatedData.WriteByte((byte)((newLength >> 8) & 0xFF));
+                            updatedData.WriteByte((byte)(newLength & 0xFF));
+                        }
+                        else
+                        {
+                            updatedData.Write(BitConverter.GetBytes(newLength), 0, 4);
+                        }
+
+                        updatedData.Write(newStringBytes, 0, newStringBytes.Length);
+                        updatedData.WriteByte(0x00);
+
+                        lastOffset = offset + 5 + originalLength;
+
+                        Console.WriteLine($"Updated lastOffset: {lastOffset}");
+                    }
+
+                    if (lastOffset < data.Length)
+                    {
+                        updatedData.Write(data, lastOffset, data.Length - lastOffset);
+                    }
+
+                    string updatedFilePath = originalFilePath + ".new";
+                    File.WriteAllBytes(updatedFilePath, updatedData.ToArray());
+
+                    Console.WriteLine("File updated and saved successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating the file: {ex.Message}.", "Error!");
+            }
+        }
+
+        private List<(int Offset, int Length, string OriginalString)> ExtractStringsWithOffsets(byte[] data)
+        {
+            List<(int Offset, int Length, string OriginalString)> stringsWithOffsets = new List<(int Offset, int Length, string OriginalString)>();
+
+            int offset = 0;
+            while (offset < data.Length - 5)
+            {
+                if (data[offset] == 0x04)
+                {
+                    int length;
+                    if (radioButton2.Checked)
+                    {
+                        length = (data[offset + 1] << 24) |
+                                 (data[offset + 2] << 16) |
+                                 (data[offset + 3] << 8) |
+                                 data[offset + 4];
+                    }
+                    else
+                    {
+                        length = BitConverter.ToInt32(data, offset + 1);
+                    }
+
+                    if (length > 0 && offset + 5 + length <= data.Length)
+                    {
+                        byte[] stringBytes = new byte[length];
+                        Array.Copy(data, offset + 5, stringBytes, 0, length);
+
+                        string decodedString = Encoding.UTF8.GetString(stringBytes).TrimEnd('\0').Replace("\n", "<lw>");
+                        if (Regex.IsMatch(decodedString, @"^[\x20-\x7E<lw>\u00A0-\uFFFF]+$"))
+                        {
+                            stringsWithOffsets.Add((offset, length, decodedString));
+                        }
+                    }
+                }
+                offset++;
+            }
+
+            return stringsWithOffsets;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tool by Alex ''OAleex'' Félix\nVersion: 1.0", "About");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton1.Checked = false;
+                radioButton3.Checked = false;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+            }
+        }
+
+
+        private void xORDecryptorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Title = "Select files to decrypt",
+                    Filter = "Lua file (*.lua)|*.lua|All files (*.*)|*.*",
+                    Multiselect = true
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string inputFile in openFileDialog.FileNames)
+                    {
+                        string outputFile = Path.Combine(
+                            Path.GetDirectoryName(inputFile),
+                            Path.GetFileNameWithoutExtension(inputFile) + "_decrypted" + Path.GetExtension(inputFile)
+                        );
+
+                        EncryptFile(inputFile, outputFile);
+                    }
+
+                    MessageBox.Show("Files decrypted successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void xOREncryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Title = "Select files to encrypt",
+                    Filter = "Lua file (*.lua)|*.lua|All files (*.*)|*.*",
+                    Multiselect = true
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string inputFile in openFileDialog.FileNames)
+                    {
+                        string outputFile = Path.Combine(
+                            Path.GetDirectoryName(inputFile),
+                            Path.GetFileNameWithoutExtension(inputFile) + "_encrypted" + Path.GetExtension(inputFile)
+                        );
+
+                        EncryptFile(inputFile, outputFile);
+                    }
+
+                    MessageBox.Show("Files encrypted successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void EncryptFile(string inputFile, string outputFile)
+        {
+            if (!File.Exists(inputFile))
+            {
+                MessageBox.Show($"Arquivo de entrada não encontrado: {inputFile}", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (var fin = new FileStream(inputFile, FileMode.Open))
+            using (var fout = new FileStream(outputFile, FileMode.Create))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = fin.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    EncryptBytes(buffer, bytesRead);
+                    fout.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        private void EncryptBytes(byte[] buffer, int count)
+        {
+            for (int i = 0; i < count; i++)
+                buffer[i] ^= XORKey;
+        }
+    }
+}
